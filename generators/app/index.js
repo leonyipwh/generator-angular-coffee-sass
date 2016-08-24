045ca7485aacaf0dca8ce199724278dbb4eeceda
Leon Yip <leonyipwh@hotmail.com>
2016-08-25 02:59:49 +0800

    imagCMS checkbox in prompt

--- generators/app/index.js
+++ generators/app/index.js
@@ -10,13 +10,33 @@ module.exports = yeoman.Base.extend({
       'Welcome to the unreal ' + chalk.red('generator-angular-coffee-sass') + ' generator!'
     ));
 
-    return this.prompt([{
-      type: 'input',
-      name: 'name',
-      message: 'Your project name',
-      default: this.appname // Default to current folder name
-    }]).then(function (answers) {
-      this.log('app name', answers.name);
+    return this.prompt([
+      {
+        type: 'input',
+        name: 'projectName',
+        message: 'Your project name',
+        default: this.appname // Default to current folder name
+      },
+      {
+        type: 'checkbox',
+        name: 'features',
+        message: 'Which additional features would you like to include?',
+        choices: [{
+          name: 'Imag CMS',
+          value: 'includeCMS',
+          checked: true
+        }]
+      },
+    ]).then(function (answers) {
+      var features = answers.features;
+
+      function hasFeature(feat) {
+        return features && features.indexOf(feat) !== -1;
+      };
+
+      this.projectName = hasFeature('projectName');
+
+      this.log('Project Name', answers.projectName);
     }.bind(this));
   },
 
@@ -24,6 +44,16 @@ module.exports = yeoman.Base.extend({
     this.fs.copy(
       this.templatePath('**/**/*.*'),
       this.destinationPath('./')
+    );
+  },
+
+  packageJSON: function () {
+    this.fs.copyTpl(
+      this.templatePath('package.json'),
+      this.destinationPath('package.json'),
+      {
+        projecName: this.projecName,
+      }
     );
   },
 
